4c0d3508985e6ef06f64cd078bae81f6daaceb62
stonevessel <157413052+stonevessel@users.noreply.github.com>
2024-02-13 14:10:52 -0500

    Initial commit

---
+++ src/AddressBook.java
@@ -0,0 +1,4 @@
+public class AddressBook {
+
+    // use a hassmapp lol 
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("chris","Christemail@email.com");
+        Person person2 = new Person("tom","tomemail@email.com");
+
+
+    }
+}
